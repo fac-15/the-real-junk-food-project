646421f7c0735edb2565a565eb35e8d99f97eff5
Whooolia <tpdms2779@gmail.com>
2019-02-07 12:56:01 +0000

    Logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Logo from './img/logo.png';
+// console.log(Logo);
 
 class App extends Component {
   render() {
-    return <h1>TRJFP</h1>;
+    return (<div>
+      <p>hello!</p>
+      <img src={Logo} alt='logo' />
+      </div>)
   }
 }
 

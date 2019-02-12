8465dddda858e0f845515c5bb6086b66337a0a7e
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-02-12 12:57:51 +0000

    Add react-testing-library and slightly refactor form test

--- src/component/form/form.test.js
+++ src/component/form/form.test.js
@@ -1,10 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render, cleanup } from "react-testing-library";
 import Form from "./form.js";
+
+//Ensures that the document gets cleared after each test
+afterEach(cleanup);
 
 describe("Form component tests", () => {
   test("The form renders", () => {
-    const root = document.createElement("div");
-    ReactDOM.render(<Form />, root);
+    render(<Form />);
   });
 });

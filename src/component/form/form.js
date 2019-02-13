d0f538a4940968877a33e5b066e432e79d44c3e5
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-02-13 12:16:45 +0000

    Basic connection from fetch call to server

--- src/component/form/form.js
+++ src/component/form/form.js
@@ -6,53 +6,60 @@ import {
   Switch,
   Redirect
 } from "react-router-dom";
-const Airtable = require("airtable");
-const base = new Airtable({ apiKey: "keyc5S01QxJ4qTMSs" }).base(
-  "appyRQ1dyAAvZyIPI"
-);
 
 class Form extends Component {
   state = {
-    email: "",
-    pin: "",
-    stuff: [],
-    hello: false
+    email: "hello@thisime.goaway",
+    pin: "1668"
   };
   handleChange = event => {
     const target = event.target;
     this.setState({ [target.name]: target.value });
   };
+  // handleSubmit = event => {
+  //   let thisPin = this.state.pin;
+  //   let thisEmail = this.state.email;
+  //   event.preventDefault();
+  //   // const data = JSON.stringify(this.state);
+  //   base("Drivers")
+  //     .select({
+  //       // Selecting the first 3 records in Grid view:
+  //       filterByFormula: `(AND({pin} = "${thisPin}", {email} = "${thisEmail}"))`,
+  //       maxRecords: 3,
+  //       view: "Grid view"
+  //     })
+  //     .eachPage(
+  //       function page(records, fetchNextPage) {
+  //         // This function (`page`) will get called for each page of records.
+  //         records.forEach(function(record) {
+  //           console.log("Retrieved", record.get("Name"));
+  //         });
+  //         // To fetch the next page of records, call `fetchNextPage`.
+  //         // If there are more records, `page` will get called again.
+  //         // If there are no more records, `done` will get called.
+  //         fetchNextPage();
+  //       },
+  //       function done(err) {
+  //         if (err) {
+  //           console.error(err);
+  //           return;
+  //         }
+  //       }
+  //     );
+  //   this.props.isAuth(true);
+  // };
   handleSubmit = event => {
-    let thisPin = this.state.pin;
-    let thisEmail = this.state.email;
     event.preventDefault();
-    // const data = JSON.stringify(this.state);
-    base("Drivers")
-      .select({
-        // Selecting the first 3 records in Grid view:
-        filterByFormula: `(AND({pin} = "${thisPin}", {email} = "${thisEmail}"))`,
-        maxRecords: 3,
-        view: "Grid view"
-      })
-      .eachPage(
-        function page(records, fetchNextPage) {
-          // This function (`page`) will get called for each page of records.
-          records.forEach(function(record) {
-            console.log("Retrieved", record.get("Name"));
-          });
-          // To fetch the next page of records, call `fetchNextPage`.
-          // If there are more records, `page` will get called again.
-          // If there are no more records, `done` will get called.
-          fetchNextPage();
-        },
-        function done(err) {
-          if (err) {
-            console.error(err);
-            return;
-          }
-        }
-      );
-    this.props.isAuth(true);
+    const data = JSON.stringify(this.state);
+    fetch("/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: data
+    })
+      .then(res => res.json())
+      .then(returnedData => console.log(returnedData));
   };
   render() {
     if (this.state.hello) {
@@ -85,11 +92,6 @@ class Form extends Component {
         <button onClick={this.handleSubmit} type="submit">
           Submit
         </button>
-        <ul>
-          {this.state.stuff.map(name => {
-            return <li>name</li>;
-          })}
-        </ul>
       </form>
     );
   }

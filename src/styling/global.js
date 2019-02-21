8bc4e7f086d010b00af467398373b62a94854ccc
Whooolia <36998110+Whooolia@users.noreply.github.com>
2019-02-21 17:53:34 +0000

    Update global.js

--- src/styling/global.js
+++ src/styling/global.js
@@ -8,9 +8,9 @@ body{
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-color: #009DA8;
-    background-image: -webkit-linear-gradient(117deg, ${props =>
-      props.invert ? "#009DA8" : "#ffffff"} 63%, ${props =>
-  props.invert ? "#ffffff" : "#009DA8"} 50% );
+    background-image: linear-gradient(155deg, ${props =>
+      props.invert ? "#ffffff" : "#009DA8"} 35%, ${props =>
+  props.invert ? "#009DA8" : "#ffffff"} 35% );
 
 p {
   margin: 0.2em;
